c10d764df89e2e2287837947588742f49a6dce6c
Simone Corsi <simonecorsi.rm@gmail.com>
2021-10-20 00:01:45 +0200

    style: adds lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '*.{js,css,json,md,yaml,yml}': ['prettier --write'],
+  '*.{js,ts,css,json,md,yaml,yml}': ['prettier --write'],
   '*.md': (filenames) => {
     const list = filenames.map((filename) => `'markdown-toc -i ${filename}`);
     return list;
   },
-  '*.js': ['eslint --fix'],
+  '*.{js,ts}': ['eslint --fix'],
 };
