6417159a6fd3acd0420c80d324491a179e7b7ed9
Simone Corsi <simonecorsi.dev@gmail.com>
2022-04-20 16:26:24 +0200

    build(eslint): fix linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 /* eslint-disable no-undef */
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    browser: false,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
 };
